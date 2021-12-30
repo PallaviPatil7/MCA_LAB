8936b5b4a16373c6a97dbc3b0241246f14b38bc0
Pallavi Patil <70833788+PallaviPatil7@users.noreply.github.com>
2021-12-30 13:45:33 +0530

    Update sourcecode_1.c

--- sourcecode_1.c
+++ sourcecode_1.c
@@ -37,12 +37,12 @@ void pll()                  //Fosc=12Mhz,CCLK=60Mhz,PCLK=60MHz
 }
 void delay(unsigned int z)
 {
-    T0CTCR=0x0;                                 //Select Timer Mode
+    T0CTCR=0x0;                 //Select Timer Mode
     T0TCR=0x00;                 //Timer off
     T0PR=59999;                 //Prescaler value for 1ms
     T0TCR=0x02;                 //Timer reset
     T0TCR=0x01;                 //Timer ON
     while(T0TC<z);          
     T0TCR=0x00;                 //Timer OFF
-    T0TC=0;                     //Clear the TC value. This is Optional.
+    T0TC=0;                     //Clear the TC value.
 }
